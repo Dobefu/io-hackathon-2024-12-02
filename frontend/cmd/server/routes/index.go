badab561b10a0e1a7214ed8a38d3f552b6fc5b4d
Connor van Spronssen <info@dobefu.com>
2024-12-02 10:30:40 +0100

    Add logic to load a basic HTML template

--- frontend/cmd/server/routes/index.go
+++ frontend/cmd/server/routes/index.go
@@ -1,10 +1,16 @@
 package routes
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 func Index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
+
+	tpl := template.Must(template.ParseFiles("cmd/templates/html.tpl.html"))
+	tpl.Execute(w, nil)
 }
