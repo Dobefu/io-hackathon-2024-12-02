f4d41b63e2ef6252f89e879042241f31f50f73d4
Connor van Spronssen <info@dobefu.com>
2024-12-02 15:02:34 +0100

    Add a basic layout

--- frontend/cmd/server/routes/index.go
+++ frontend/cmd/server/routes/index.go
@@ -11,6 +11,14 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	tpl := template.Must(template.ParseFiles("cmd/templates/html.tpl.html"))
-	tpl.Execute(w, nil)
+	tpl := template.Must(template.ParseFiles(
+		"cmd/templates/html.tpl.html",
+		"cmd/templates/layout/header.tpl.html",
+		"cmd/templates/layout/footer.tpl.html",
+	))
+	err := tpl.Execute(w, nil)
+
+	if err != nil {
+		http.NotFound(w, r)
+	}
 }
