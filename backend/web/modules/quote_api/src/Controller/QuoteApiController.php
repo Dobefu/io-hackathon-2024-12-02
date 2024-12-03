afc6b3be5003bd3d7f9f849e6b9bc334c6505552
Thomas van der Velde <contact@toolbarthomas.net>
2024-12-03 10:41:43 +0100

    update: Implements initial quotes api controller

---
+++ backend/web/modules/quote_api/src/Controller/QuoteApiController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Drupal\quote_api\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Drupal\node\Entity\Node;
+
+/**
+ * Controller for the Quote API.
+ */
+class QuoteApiController
+{
+
+  /**
+   * Returns the available entries from the `quote` content type.
+   *
+   * @param \Symfony\Component\HttpFoundation\Request $request
+   *  The initial HTTP Request.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *  Expected JSON Response containing quote data.
+   */
+  public function getQuotes(Request $request)
+  {
+    // Fetch published quotes of content type 'quote'.
+    $query = \Drupal::entityQuery('node')
+      ->condition('type', 'quote')
+      ->condition('status', 1);
+
+    $nodes = $query->execute();
+    $quotes = Node::loadMultiple($nodes);
+
+    $response = [];
+    foreach ($quotes as $quote) {
+      $person = $quote->get('field_person')->entity;
+      $response[] = [
+        'id' => $quote->id(),
+        'title' => $quote->getTitle(),
+        'body' => $quote->get('body')->value,
+        'person' => $person ? $person->getName() : null,
+      ];
+    }
+
+    return new JsonResponse($response);
+  }
+}
