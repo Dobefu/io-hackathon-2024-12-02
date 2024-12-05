b6470bc9c3bb7b9ab19dc51950768a35c47fc0ae
Connor van Spronssen <info@dobefu.com>
2024-12-05 07:12:56 +0100

    Add a robots.txt file

--- frontend/cmd/server/handle-routes.go
+++ frontend/cmd/server/handle-routes.go
@@ -7,6 +7,7 @@ import (
 
 func HandleRoutes(mux *http.ServeMux) {
 	mux.HandleFunc("GET /static/{path...}", routes.StaticFiles)
+	mux.HandleFunc("GET /robots.txt", routes.RobotsTxt)
 	mux.HandleFunc("GET /quotes", routes.QuotesOverview)
 	mux.HandleFunc("GET /quote/{id}", routes.QuoteById)
 
