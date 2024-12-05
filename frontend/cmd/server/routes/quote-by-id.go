71e831fd3e51911294dbebd3e49dbf15a8ca581c
Connor van Spronssen <info@dobefu.com>
2024-12-05 08:42:48 +0100

    Add logic to exclude the current quote from the related quotes

--- frontend/cmd/server/routes/quote-by-id.go
+++ frontend/cmd/server/routes/quote-by-id.go
@@ -26,7 +26,21 @@ func QuoteById(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	relatedQuotes, err := getQuotesByPerson(quote["person"].(string))
+	quotesByPerson, err := getQuotesByPerson(quote["person"].(string))
+
+	var relatedQuotes []interface{}
+
+	for _, quoteByPerson := range quotesByPerson {
+		if quote["id"] == quoteByPerson.(map[string]interface{})["id"] {
+			continue
+		}
+
+		if len(relatedQuotes) >= 3 {
+			break
+		}
+
+		relatedQuotes = append(relatedQuotes, quoteByPerson)
+	}
 
 	if err != nil {
 		log.Println(err.Error())
@@ -69,7 +83,7 @@ func getQuoteById(id string) (map[string]interface{}, error) {
 	return output[0].(map[string]interface{}), nil
 }
 
-func getQuotesByPerson(person string) (interface{}, error) {
+func getQuotesByPerson(person string) ([]interface{}, error) {
 	endpoint := os.Getenv("API_ENDPOINT")
 	url := fmt.Sprintf("%s/quote/get?token=%s", endpoint, os.Getenv("API_KEY"))
 
@@ -86,7 +100,7 @@ func getQuotesByPerson(person string) (interface{}, error) {
 
 	defer response.Body.Close()
 
-	var output interface{}
+	var output []interface{}
 	err = json.NewDecoder(response.Body).Decode(&output)
 
 	if err != nil {
