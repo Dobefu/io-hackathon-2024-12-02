7962979296a7b21db101f894ef32ce611e86b2d9
Connor van Spronssen <info@dobefu.com>
2024-12-05 08:26:17 +0100

    Add related quotes

--- frontend/cmd/server/routes/quote-by-id.go
+++ frontend/cmd/server/routes/quote-by-id.go
@@ -8,12 +8,14 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func QuoteById(w http.ResponseWriter, r *http.Request) {
 	templates := utils.CollectGlobalTemplates()
 	templates = append(templates, "cmd/templates/pages/quote.html.tmpl")
 	templates = append(templates, "cmd/templates/partials/quote.html.tmpl")
+	templates = append(templates, "cmd/templates/partials/quote--teaser.html.tmpl")
 
 	id := r.PathValue("id")
 	quote, err := getQuoteById(id)
@@ -24,8 +26,17 @@ func QuoteById(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	relatedQuotes, err := getQuotesByPerson(quote["person"].(string))
+
+	if err != nil {
+		log.Println(err.Error())
+		http.NotFound(w, r)
+		return
+	}
+
 	data := make(map[string]interface{})
 	data["Quote"] = quote
+	data["RelatedQuotes"] = relatedQuotes
 
 	tpl := template.Must(template.ParseFiles(templates...))
 	err = tpl.Execute(w, &data)
@@ -36,7 +47,7 @@ func QuoteById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func getQuoteById(id string) (interface{}, error) {
+func getQuoteById(id string) (map[string]interface{}, error) {
 	endpoint := os.Getenv("API_ENDPOINT")
 	url := fmt.Sprintf("%s/quote/get/%s?token=%s", endpoint, id, os.Getenv("API_KEY"))
 
@@ -55,5 +66,32 @@ func getQuoteById(id string) (interface{}, error) {
 		return nil, err
 	}
 
-	return output[0], nil
+	return output[0].(map[string]interface{}), nil
+}
+
+func getQuotesByPerson(person string) (interface{}, error) {
+	endpoint := os.Getenv("API_ENDPOINT")
+	url := fmt.Sprintf("%s/quote/get?token=%s", endpoint, os.Getenv("API_KEY"))
+
+	if person != "" {
+		person = strings.ReplaceAll(person, " ", "%20")
+		url = fmt.Sprintf("%s&person=%s", url, person)
+	}
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	var output interface{}
+	err = json.NewDecoder(response.Body).Decode(&output)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output, nil
 }
