e41dc85754a1f810717dbc94685418ec86918da6
Connor van Spronssen <info@dobefu.com>
2024-12-06 11:04:18 +0100

    Collect a random quote on the homepage

--- frontend/cmd/server/routes/index.go
+++ frontend/cmd/server/routes/index.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"encoding/json"
+	"fmt"
 	"frontend/cmd/server/utils"
 	"html/template"
+	"log"
 	"net/http"
+	"os"
 )
 
 func Index(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +19,45 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	templates := utils.CollectGlobalTemplates()
 	templates = append(templates, "cmd/templates/pages/index.html.tmpl")
 
-	tpl := template.Must(template.ParseFiles(templates...))
-	err := tpl.Execute(w, nil)
+	quote, err := getRandomQuote()
 
 	if err != nil {
+		log.Println(err.Error())
 		http.NotFound(w, r)
+		return
 	}
+
+	data := make(map[string]interface{})
+	data["Quote"] = quote
+
+	tpl := template.Must(template.ParseFiles(templates...))
+	err = tpl.Execute(w, nil)
+
+	if err != nil {
+		log.Println(err.Error())
+		http.NotFound(w, r)
+		return
+	}
+}
+
+func getRandomQuote() (map[string]interface{}, error) {
+	endpoint := os.Getenv("API_ENDPOINT")
+	url := fmt.Sprintf("%s/quote/random?token=%s", endpoint, os.Getenv("API_KEY"))
+
+	response, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer response.Body.Close()
+
+	var output interface{}
+	err = json.NewDecoder(response.Body).Decode(&output)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return output.(map[string]interface{}), nil
 }
